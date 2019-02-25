8422b4e1618bdf7ad4a4a681586991b138939dc4
Ryan <rb646984@sju.edu>
2019-02-25 17:34:23 -0500

    Basic Game Board Visual

--- lib/mainarray.dart
+++ lib/mainarray.dart
@@ -3,23 +3,28 @@ import 'package:flutter/material.dart';
 int x = 0;
 int y = 0;
 
-List<List<bool>> grid = new List.generate(5, (i) => new List(9));
+//Generates initial board
+//Needs to account for different halves
+List<List<String>> grid = new List.generate(9, (i) => new List(5));
 void generateBoard(){
   for (int i = 0; i < grid.length; i++) {
     for (int j = 0; j < grid[0].length; j++) {
-      grid[i][j] = false;
+      grid[i][j] = "o";
     }
   }
 }
 
 void playerSpawn(){
-  grid[3][0] = true;
+  grid[0][2] = "x";
+  x = 0;
+  y = 2;
 }
 
+//To be implemented to find play position potentially replacing x and y variables
 bool playerLocation(){
   for (int i = 0; i < grid.length; i++) {
     for (int j = 0; j < grid[0].length; j++) {
-      if(grid[i][j] == true){
+      if(grid[i][j] == "x"){
         x = i;
         y = j;
         return true;
@@ -41,15 +46,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '2D Array',
-      home: MyHomePage(title: 'Main'),/*Scaffold(
-        appBar: AppBar(
-          title: Text('Board 2D Array'),
-        ),
-        body: Center(
-          child: Text("The player is currently at " + x.toString() + ", " +
-              y.toString() + "."),
-        ),
-      ),*/
+      home: MyHomePage(title: 'Main'),
     );
   }
 }
@@ -63,36 +60,52 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  //Move functions move the player based on direction of function
+  //Replaces previous spot player was on with o, changes new tile player is on to x
+  //x and y are currently reversed due to a mistake in representing the x and y axis
+  //Changing x value moves up and down, y moves right to left
   void moveUp(){
     setState(() {
-      grid[x][y] = false;
-      grid[x][y+1] = true;
+      grid[x][y] = "o";
+      grid[x+1][y] = "x";
       playerLocation();
     });
   }
 
   void moveDown(){
     setState(() {
-      grid[x][y] = false;
-      grid[x][y-1] = true;
+      grid[x][y] = "o";
+      grid[x-1][y] = "x";
       playerLocation();
     });
   }
 
   void moveLeft(){
     setState(() {
-      grid[x][y] = false;
-      grid[x-1][y] = true;
+      grid[x][y] = "o";
+      grid[x][y+1] = "x";
       playerLocation();
     });
   }
 
   void moveRight(){
     setState(() {
-      grid[x][y] = false;
-      grid[x+1][y] = true;
+      grid[x][y] = "o";
+      grid[x][y-1] = "x";
       playerLocation();
     });
+  }
+
+  //Gives the board a (temporary) way of viewing the layout
+  String board2String(){
+    String board = "";
+    for (int i = 8; i >= 0; i--) {
+      for (int j = 4; j >= 0; j--) {
+        board += grid[i][j] + "  ";
+      }
+      board += "\n";
+    }
+    return board;
   }
 
   @override
@@ -105,8 +118,9 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text("The player is currently at " + x.toString() + ", " +
-              y.toString() + "."),
+              Text(board2String(),
+                textScaleFactor: 2.0, //Changes the font size  better way?
+              ),
               FlatButton(
                 onPressed: moveUp,
                 child: Column(
@@ -116,9 +130,10 @@ class _MyHomePageState extends State<MyHomePage> {
                   ],
                 ),
               ),
-              Row(
+              Row(  //Contains move buttons left, down, and up displayed like keyboard arrow keys
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                FlatButton(
+                FlatButton( //Left Button
                   onPressed: moveLeft,
                   child: Column(
                     children: <Widget>[
@@ -127,7 +142,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     ],
                   ),
                 ),
-                FlatButton(
+                FlatButton( //Down Button
                   onPressed: moveDown,
                   child: Column(
                     children: <Widget>[
@@ -136,7 +151,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     ],
                   ),
                 ),
-                FlatButton(
+                FlatButton( //Right Button
                   onPressed: moveRight,
                   child: Column(
                     children: <Widget>[
